9106e38bd1ebf629217a450b14a24928c46cb208
cz1501 <122151441+cz1501@users.noreply.github.com>
2024-10-11 09:25:33 -0700

    Allow CORS

--- server.js
+++ server.js
@@ -52,10 +52,10 @@ http
     const path = parsedUrl.pathname;
 
     // Add CORS headers
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    
     // Handle preflight OPTIONS request
     if (req.method === 'OPTIONS') {
     res.writeHead(200);
